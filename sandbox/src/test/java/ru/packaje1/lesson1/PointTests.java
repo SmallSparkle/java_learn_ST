dc662047b564b05c9dfda8fb5f90aa301649baf2
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-01-31 13:38:42 +0100

    изменения в ДЗ 1.2

--- sandbox/src/test/java/ru/packaje1/lesson1/PointTests.java
+++ sandbox/src/test/java/ru/packaje1/lesson1/PointTests.java
@@ -1,12 +1,8 @@
 package ru.packaje1.lesson1;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static ru.packaje1.lesson1.Point.distance;
-
 public class PointTests {
-
   @Test
   public void SquareTests() {
     Point p = new Point(1.0, 2.0);
@@ -27,14 +23,15 @@ public class PointTests {
     Point p = new Point(-10.0, -7.5);
     Point p1 = new Point(-0.1, -6.0);
     Assert.assertEquals(p.distance(p1), 10.012991560967182);
-  }//тест статичного метода - падает т.к. ожидание указано не верно
+  }
 
   @Test
   public void SquareTestsStatic() {
     Point p = new Point(1.0, 2.0);
     Point p1 = new Point(3.0, 5.0);
     Assert.assertEquals(distance(p, p1), 3.60555127546398);
-  }
+  }//тест статичного метода - падает т.к. ожидание указано не верно
+
   @Test
   public void SquareTestsStaticPasst() {
     Point p = new Point(1.0, 12.0);
