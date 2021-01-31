da67723ca2ac72b0e5571bc2353ee539bda30bc8
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-01-31 13:30:17 +0100

    ДЗ 1.2 изменения

--- sandbox/src/main/java/ru/packaje1/lesson1/ShowDistanceBPoints.java
+++ sandbox/src/main/java/ru/packaje1/lesson1/ShowDistanceBPoints.java
@@ -4,8 +4,8 @@ import static ru.packaje1.lesson1.Point.distance;
 
 public class ShowDistanceBPoints {
   public static void main(String[] args) {
-    Point p = new Point(1.0,2.0);
-    Point p1 = new Point(3.0,5.0);
+    Point p = new Point(1.0, 12.0);
+    Point p1 = new Point(44.0, 5.0);
     System.out.println("Расстояние между точками = "+distance(p,p1));
   //вызов статичного метода
 
--- sandbox/src/test/java/ru/packaje1/lesson1/PointTests.java
+++ sandbox/src/test/java/ru/packaje1/lesson1/PointTests.java
@@ -16,10 +16,29 @@ public class PointTests {
   }
 
   @Test
+  public void SquareTestsNull() {
+    Point p = new Point(3.0, 4.0);
+    Point p1 = new Point(3.0, 4.0);
+    Assert.assertEquals(p.distance(p1), 0.0);
+  }
+
+  @Test
+  public void SquareTestsNegativeNumber() {
+    Point p = new Point(-10.0, -7.5);
+    Point p1 = new Point(-0.1, -6.0);
+    Assert.assertEquals(p.distance(p1), 10.012991560967182);
+  }//тест статичного метода - падает т.к. ожидание указано не верно
+
+  @Test
   public void SquareTestsStatic() {
     Point p = new Point(1.0, 2.0);
     Point p1 = new Point(3.0, 5.0);
     Assert.assertEquals(distance(p, p1), 3.60555127546398);
-    //тест статичного метода - падает т.к. ожидание указано не верно
+  }
+  @Test
+  public void SquareTestsStaticPasst() {
+    Point p = new Point(1.0, 12.0);
+    Point p1 = new Point(44.0, 5.0);
+    Assert.assertEquals(distance(p, p1), 43.56604182158393);
   }
 }
