be5c46b4a64d6962c7d80f06e8a1672e84a70f95
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-02-07 16:41:02 +0100

    navigator helper added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+  private WebDriver wd;
+
+  public NavigationHelper(WebDriver wd) {
+    this.wd = wd;
+  }
+
+  public void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+  public void goToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  public void goToContactCreatForm(String s) {
+    wd.findElement(By.linkText(s)).click();
+  }
+}
