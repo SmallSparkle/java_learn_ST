193263b2f7e1a80aeb43717b6a08d985f70204fd
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-02-07 17:31:27 +0100

    ContactHelper added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+  public ContactHelper(WebDriver wd) {
+    super(wd);
+  }
+
+  public void submitContactCreation() {
+    click(By.xpath("(//input[@name='submit'])[2]"));
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    type(By.name("firstname"), contactData.getName());
+    type(By.name("middlename"), contactData.getMiddlename());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("address"), contactData.getAddress());
+    type(By.name("home"), contactData.getHomePhone());
+    type(By.name("mobile"), contactData.getMobilePhone());
+    type(By.name("email"), contactData.getFerstEmail());
+    type(By.name("email3"), contactData.getThirdEmail());
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
+    type(By.name("byear"), contactData.getByear());
+    wd.findElement(By.name("new_group")).click();
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getAddGroup());
+    type(By.name("notes"), contactData.getNotesText());
+
+  }
+}
