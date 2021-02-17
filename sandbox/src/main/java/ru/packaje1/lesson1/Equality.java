b3ff824b2ef68186aa7fa2303f0b8554bf96e408
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-02-17 10:30:37 +0100

    lesson 3 exam

---
+++ sandbox/src/main/java/ru/packaje1/lesson1/Equality.java
@@ -0,0 +1,11 @@
+package ru.packaje1.lesson1;
+
+public class Equality {
+  public static void main(String[] args) {
+    String s1 = "firefix";
+    String s2 = s1;
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+
+  }
+}
