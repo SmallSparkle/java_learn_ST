8267d145a7996ff40bf8ea29dfc666821da73782
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-02-23 12:41:36 +0100

    lesson4.1

--- sandbox/src/test/java/lesson4/PrimesTests.java
+++ sandbox/src/test/java/lesson4/PrimesTests.java
@@ -1,4 +1,27 @@
 package lesson4;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PrimesTests {
+  @Test
+  public void testPrimes() {
+    Assert.assertTrue(Primes.isPrimes(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNotPrimes() {
+    Assert.assertFalse(Primes.isPrimes(Integer.MAX_VALUE - 2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimesLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrimes(n));
+  }
+
+  @Test
+  public void testNotPrimesLong() {
+    Assert.assertFalse(Primes.isPrimes(Integer.MAX_VALUE - 2));
+  }
 }
