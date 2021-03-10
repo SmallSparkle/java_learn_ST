99269546745894be7f240151f5621b8ee6929976
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-03-10 14:12:10 +0100

    added Set<> end id parsing to GroupCreationTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -8,6 +8,19 @@ public class GroupData {
   private String header;
   private String footer;
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id && Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name);
+  }
+
 //  public GroupData(int id, String name, String header, String footer) {
 //    this.id = id;
 //    this.name = name;
@@ -65,19 +78,6 @@ public class GroupData {
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
   }
 
 }
