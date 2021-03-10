49c1565dbe435f12fc649c2da7b7b124769eace5
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-03-10 11:33:22 +0100

    added Builder for group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -7,18 +7,18 @@ import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.Comparator;
 import java.util.List;
 
-public class GroupCreationTests extends TestBase {
+public class  GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test4", "test2", "TestTEstTEST");
+    GroupData group = new GroupData().withName("test1").withHeader("TEST2").withFooter("some text");
     app.group().createGroup(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -14,7 +14,7 @@ public class GroupModificationTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().createGroup(new GroupData("test1", null, null));
+      app.group().createGroup(new GroupData().withName("test1"));
     }
   }
 
@@ -23,7 +23,10 @@ public class GroupModificationTests extends TestBase {
 
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(), "modification", "test modif", "Test");
+    GroupData group = new GroupData()
+            .withId(before.get(index).getId())
+            .withName("modification")
+            .withHeader("test modif").withFooter("Test");
     app.group().modifyGroup(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());
