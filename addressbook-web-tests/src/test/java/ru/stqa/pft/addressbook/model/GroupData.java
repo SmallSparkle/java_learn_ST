2bf4c5d1c0f96d3ff28ce31f527bb20fdadc8468
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-03-19 22:19:38 +0100

    added json generator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,16 +1,20 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.util.Objects;
 
- @XStreamAlias("group")
+@XStreamAlias("group")
 public class GroupData {
-   @XStreamOmitField
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;
+  @Expose
   private String name;
+  @Expose
   private String header;
+  @Expose
   private String footer;
 
   @Override
