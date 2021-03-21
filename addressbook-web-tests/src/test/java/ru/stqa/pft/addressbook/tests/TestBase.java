d6c65c538aae0754187d599eac235b3ad802131f
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-03-21 13:17:48 +0100

    added browser config to TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
-
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser"));
+        //(BrowserType.FIREFOX);
 
   @BeforeSuite
   public void setUp() throws Exception {
