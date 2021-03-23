e0dcef22235c6e5efed01f2ebfed5cd7dbb0e06e
SmallSparkle <77972389+SmallSparkle@users.noreply.github.com>
2021-03-23 09:49:36 +0100

    added db chek to BeforeMethod to ContactDataTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -15,20 +16,20 @@ public class ContactDataTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
       app.group().createGroup(new GroupData().withName("test1"));
     }
-    app.goTo().homePage();
-    if (app.contact().all().size() == 0) {
+    if (app.db().contacts().size() == 0) {
+      app.goTo().homePage();
       app.contact().createContact(new ContactData()
               .withName("Anna").withMiddlename("Amina").withLastname("Bespalova")
+              .withPhoto(new File("src/test/resources/asd.jpg"))
               .withAddress("Moscow Lenina 10").withHomePhone("4959880012")
               .withMobilePhone("9660001213").withFerstEmail("some@some.mail")
               .withThirdEmail("some2@some.mail").withBday("5").withBmonth("May")
               .withByear("1987").withNotesText("test notes"), true);
     }
-    app.goTo().homePage();
   }
 
   @Test
